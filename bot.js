073ac4b8692cc50ff6fa91a1e8b8f4fa9c612f46
knifeworm <98216468+Imbored24@users.noreply.github.com>
2022-02-07 15:13:44 +0000

    Made a command handler cleaned up some of the code into different files

--- bot.js
+++ bot.js
@@ -1,11 +1,5 @@
-const discord = require('discord.js');
-// Tick all intents in discord.com/developers
-const client = new discord.Client({ intents: 32767 });
-const { token } = require('./config.json');
+const Client = require('./Structures/Client');
+const config = require('./config.json');
 
-client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`)
-});
-
-
-client.login(token);
+const client = new Client(config);
+client.start();

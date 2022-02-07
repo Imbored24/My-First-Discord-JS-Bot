c438a65986224591ac0104621f119593a587604a
knifeworm <98216468+Imbored24@users.noreply.github.com>
2022-02-07 14:20:55 +0000

    Deleted everything, gonna rewrite it i was not happy of how it was written

--- bot.js
+++
@@ -1,7 +0,0 @@
-const { Client } = require("discord.js");
-const client = new Client({intents: 32767});
-const {token} = require('./config.json');
-
-require("./Handlers/Events")(client);
-
-client.login(token);

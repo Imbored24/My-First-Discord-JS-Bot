c8b19d1ef066d4c9f38cdf7b44743246821d51a7
knifeworm <98216468+Imbored24@users.noreply.github.com>
2022-02-07 15:21:23 +0000

    Beautified Commands\hello.js

--- Commands/hello.js
+++ Commands/hello.js
@@ -1,13 +1,13 @@
 const Command = require('../Structures/Command.js');
 
 module.exports = class extends Command {
-    constructor(...args){
+    constructor(...args) {
         super(...args, {
             aliases: ['hallo']
         });
     }
 
-    async run(message, args){
+    async run(message, args) {
         message.channel.send("Hello!");
     }
-} 
+}
